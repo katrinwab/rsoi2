83223c49b8af9b5c9798116f5e957a52856d74f1
katrinwab <katrinwab@gmail.com>
2016-02-24 17:03:08 +0300

    fix bags

--- app.js
+++ app.js
@@ -2,14 +2,11 @@ var express = require('express')
   , passport = require('passport')
   , site = require('./site')
   , oauth2 = require('./oauth2')
+  , api = require('./api')
   , util = require('util')
   , request = require('request')
-  , MagazineModel = require('./db/mongoose').MagazineModel
-  , PublishingHouseModel = require('./db/mongoose').PublishingHouseModel
-
-var methodOverride  = require('method-override');
-var log             = require('./db/log')(module);
-var fs              = require('fs');
+  , methodOverride  = require('method-override')
+  , log = require('./db/log')(module);
   
 var app = express.createServer();
 app.set('view engine', 'ejs');
@@ -25,17 +22,24 @@ app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 
 require('./auth');
 
-app.get('/mainHTML', function(req, res) {
-    file = fs.readFileSync('mainHTML.html');
-    res.end(file);
-
-})
-
 app.get('/main', site.index);
 app.get('/',function(req,res){
     var code = req.query.code;
     if (code) {
-        res.redirect('http://localhost:3000/dialog/authorize?code='+code);
+        request
+            .post('http://localhost:3000/oauth/token', {form: {grant_type: 'authorization_code', client_id: 'first',
+                client_secret: 'abc123456', redirect_uri: '/', code: code}},
+                function (err, response, body) {
+                    if (err) {
+                        res.statusCode = 500;
+                        return res.send({error: 'Ошибка сервера'});
+                    }
+                    var obj = JSON.parse(body);
+                    log.info("TOKEN %s!!", obj.access_token);
+                    res.cookie('token', obj.access_token);
+                    res.statusCode = 200;
+                    return res.send({status: 'Token was saved'});
+            })
     }
     else {
         res.redirect('http://localhost:3000/main');
@@ -51,111 +55,32 @@ app.get('/account', site.account);
 
 
 app.get('/dialog/authorize',function(req,res){
-    var code = req.query.code;
-    if (!code)
-        res.redirect('http://localhost:3000/dialog/authorize/code?response_type=code&client_id=first&redirect_uri=/');
-    else {
-        var str = '&code' + code;
-        res.redirect('http://localhost:3000/dialog/authorize/token?response_type=token&client_id=first&client_secret=abc123456&redirect_uri=/'+str);
-    }
+    res.redirect('http://localhost:3000/dialog/authorize/code?response_type=code&client_id=first&redirect_uri=/');
 });
 
 app.get('/dialog/authorize/code', oauth2.authorization);
-app.get('/dialog/authorize/token', oauth2.authorization);
 app.post('/dialog/authorize/decision', oauth2.decision);
 
 app.post('/oauth/token', oauth2.token);
 
-app.get('/api/magazines', function(req, res) {
-    MagazineModel.find( function(err, magazine) {
-        var magazineCount = magazine.length,
-            pageSize = 3,
-            pageCount = parseInt(magazine.length/3, 10)+ 1,
-            currentPage = 1,
-            magazinesArrays = [];
-
-        while (magazine.length > 0) {
-            magazinesArrays.push(magazine.splice(0, pageSize));
-        }
-
-        if (typeof req.query.page !== 'undefined') {
-            currentPage = +req.query.page;
-        }
-
-        var magazinesList = magazinesArrays[+currentPage - 1];
-        if (!err) {
-            res.render('allMagazines', {
-                magazines: magazinesList,
-                pageSize: pageSize,
-                totalStudents: magazineCount,
-                pageCount: pageCount,
-                currentPage: currentPage
-            });
-        } else {
-            res.statusCode = 500;
-            log.error('Internal error(%d): %s',res.statusCode,err.message);
-            return res.send({ error: 'Server error' });
-        }
-    });
-});
-
-
-app.get('/api/magazine/:name?', function(req, res) {
-    return MagazineModel.findOne({name : req.params.name}, function (err, magazine) {
-        if(!magazine) {
-            res.statusCode = 404;
-            return res.send({ error: 'Not found'});
-        }
-        if (!err) {
-            return res.send({ status: 'OK', magazine:magazine });
-        } else {
-            res.statusCode = 500;
-            log.error('Internal error(%d): %s',res.statusCode,err.message);
-            return res.send({ error: 'Server error' });
-        }
-    });
-});
+app.get('/api/magazines', api.getAllMagazines);
+app.get('/api/magazine/:name?', api.findMagazine);
+app.put('/api/magazine/:name?', passport.authenticate('http-header-token', { session: false }), api.changeDescription);
+app.delete('/api/magazine/:name?', passport.authenticate('http-header-token', { session: false }), api.deleteMagazine);
+app.post('/api/publishingHouse', passport.authenticate('http-header-token', { session: false }), api.addNewMagazine);
 
 app.put('/test/magazine/:name?', function (req, res) {
-    var request = require('request');
     request({
-        url: 'http://localhost:3000/api/magazine/'+ req.params.name + '?_method=PUT' + '&description=' + req.body.description,
-        method: "POST",
-        headers: {"content-type": "application/json", "Authorization": 'Token ' + req.cookies.token}},
+            url: 'http://localhost:3000/api/magazine/'+ req.params.name + '?_method=PUT' + '&description=' + req.body.description,
+            method: "POST",
+            headers: {"content-type": "application/json", "Authorization": 'Token ' + req.cookies.token}},
         function (error, response, body) {
             res.send(body);
         }
     );
 })
 
-app.put('/api/magazine/:name?', passport.authenticate('http-header-token', { session: false }), function (req, res){
-    return MagazineModel.findOne({name : req.params.name}, function (err, magazine) {
-        if(!magazine) {
-            res.statusCode = 404;
-            return res.send({ error: 'Not found' });
-        }
-
-        magazine.description = req.query.description;
-        return magazine.save(function (err) {
-            if (!err) {
-                log.info("magazine updated");
-                return res.send({ status: 'OK', magazine:magazine });
-            } else {
-                if(err.name == 'ValidationError') {
-                    res.statusCode = 400;
-                    res.send({ error: 'Validation error' });
-                } else {
-                    res.statusCode = 500;
-                    res.send({ error: 'Server error' });
-                }
-                log.error('Internal error(%d): %s',res.statusCode,err.message);
-            }
-        });
-    });
-});
-
 app.delete('/test/magazine/:name?', function (req, res) {
-    var request = require('request');
     request({
             url: 'http://localhost:3000/api/magazine/'+ req.params.name +'?_method=DELETE&name=' ,
             method: "POST",
@@ -166,27 +91,7 @@ app.delete('/test/magazine/:name?', function (req, res) {
     );
 })
 
-app.delete('/api/magazine/:name?', passport.authenticate('http-header-token', { session: false }), function (req, res){
-    return MagazineModel.findOne({name : req.params.name}, function (err, magazine) {
-        if(!magazine) {
-            res.statusCode = 404;
-            return res.send({ error: 'Not found' });
-        }
-        return magazine.remove(function (err) {
-            if (!err) {
-                log.info("magazine removed");
-                return res.send({ status: 'OK' });
-            } else {
-                res.statusCode = 500;
-                log.error('Internal error(%d): %s',res.statusCode,err.message);
-                return res.send({ error: 'Server error' });
-            }
-        });
-    });
-});
-
 app.post('/test/publishingHouse', function (req, res) {
-    var request = require('request');
     request({
             url: 'http://localhost:3000/api/publishingHouse?nameMagazine=' + req.body.nameMagazine,
             method: "POST",
@@ -196,28 +101,5 @@ app.post('/test/publishingHouse', function (req, res) {
         }
     );
 })
-
-app.post('/api/publishingHouse', passport.authenticate('http-header-token', { session: false }), function (req, res){
-    if (!req.isAuthenticated()) {
-        res.statusCode = 401;
-        log.error('Ошибка(%d): %s',res.statusCode,"Доступ запрещен");
-        return res.send({ error: 'Доступ запрещен' });
-    }
-    else {
-        return PublishingHouseModel.findOne({users: req.user.username}, function (err, house) {
-            if (!house) {
-                res.statusCode = 404;
-                return res.send({error: 'Для этого пользователя издательский дом не найден'});
-            }
-            house.magazines.push(req.query.nameMagazine);
-            var magazine = new MagazineModel({ name: req.query.nameMagazine, pubHouseName: house.name});
-            magazine.save(function(err, user) {
-                if(err) return log.error(err);
-                else log.info("New magazine - %s:%s",magazine.name, magazine.pubHouseName);
-                return res.send({status: 'OK'});
-            });
-        });
-    }
-});
 
 app.listen(3000);
